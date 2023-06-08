7ff0925c2c5d1c677134f7a106eaa22542d775e9
rkpatra201 <rabindrapatra825@gmail.com>
2023-06-09 00:59:30 +0530

    code finish

--- src/main/java/com/example/feedbackapp/FeedbackAppApplication.java
+++ src/main/java/com/example/feedbackapp/FeedbackAppApplication.java
@@ -1,13 +1,41 @@
 package com.example.feedbackapp;
 
+import com.example.feedbackapp.dao.ProductRepository;
+import com.example.feedbackapp.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
-public class FeedbackAppApplication {
+public class FeedbackAppApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeedbackAppApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+
+		productRepository.save(getProduct());
+		productRepository.save(getProduct());
+
+		System.out.println(productRepository.findAll());
+	}
+
+	private Product getProduct(){
+		Product p = new Product();
+		p.setImage("IMAGE_1");
+		p.setName("NAME1");
+		p.setPrice(23.4f);
+		p.setQuantity(2);
+		p.setUserId(UUID.randomUUID().toString());
+		return p;
+	}
 }
